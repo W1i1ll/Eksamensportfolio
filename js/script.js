77d77d317c72dc0406b3e70d49171cee55d9f0ce
W1i1ll <wisa0001@stud.kea.dk>
2024-01-09 15:07:22 +0100

    modified:   imgs/emnesite.webp 	deleted:    imgs/emnesitem.webp 	modified:   imgs/prototype.webp 	modified:   imgs/tema4.webp 	new file:   imgs/virksomhedsite.webp 	modified:   js/script.js 	modified:   ommig.html 	modified:   portfolio.css 	modified:   portfolio.html 	modified:   style.css

--- js/script.js
+++ js/script.js
@@ -13,3 +13,22 @@ document.querySelectorAll(".nav-link").forEach(link => {
         navMenu.classList.remove("active");
     });
 });
+
+// // BurgerMenu
+// const burgermenu = document.querySelector(".burgermenu");  // Vælg det element, der repræsenterer burgermenuen
+// const navMenu = document.querySelector(".nav-menu");  // Vælg det element, der indeholder navigationsmenuen
+
+// burgermenu.addEventListener("click", () => {
+//     // Lyt efter klik på burgermenuen og udfør følgende funktion:
+//     burgermenu.classList.toggle("active");  // Skift tilstanden (tilføj eller fjern) klassen "active" på burgermenuen
+//     navMenu.classList.toggle("active");  // Skift tilstanden (tilføj eller fjern) klassen "active" på navigationsmenuen
+// });
+
+// document.querySelectorAll(".nav-link").forEach(link => {
+//     // Vælg alle elementer med klassen "nav-link" og udfør følgende funktion for hvert element:
+//     link.addEventListener("click", () => {
+//         // Lyt efter klik på et navigationslink og udfør følgende funktion:
+//         burgermenu.classList.remove("active");  // Fjern klassen "active" fra burgermenuen
+//         navMenu.classList.remove("active");  // Fjern klassen "active" fra navigationsmenuen
+//     });
+// });
